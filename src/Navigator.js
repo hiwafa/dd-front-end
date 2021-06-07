f440938f5928a6f2ed5ff870ab45e7b2260f2051
Muhammad Wafa <iamprogrammercoder@gmail.com>
2021-06-07 19:15:49 +0430

    built a simple archeticture plus fixed removing header

--- src/Navigator.js
+++ src/Navigator.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import Login from './screens/Login';
-import Register from './screens/Register';
+import Login from "./screens/login";
+import Register from "./screens/register";
+import Profile from "./screens/profile";
 
 
 const Stack = createStackNavigator();
 
 export default () => {
+
     return (
         <NavigationContainer
-            screenOptions={{
-                headerShown: false
-            }}
+
         >
-            <Stack.Navigator initialRouteName="Login">
+            <Stack.Navigator
+                screenOptions={{
+                    headerShown: false
+                }}
+                initialRouteName="Login"
+            >
                 <Stack.Screen name="Login" component={Login} />
                 <Stack.Screen name="Register" component={Register} />
+                <Stack.Screen name="Profile" component={Profile} />
             </Stack.Navigator>
         </NavigationContainer>
     );
