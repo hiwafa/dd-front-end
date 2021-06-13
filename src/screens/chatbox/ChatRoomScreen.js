e69083242c0ce2c8ac893b950f887a3774362e41
Julia <julwa850@student.liu.se>
2021-06-13 18:40:32 +0200

    fixed merge conflict with package.json

--- src/screens/chatbox/ChatRoomScreen.js
+++ src/screens/chatbox/ChatRoomScreen.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import { Text, FlatList, View, StyleSheet } from 'react-native';
-import { useRoute } from '@react-navigation/native'
-import chats from '../../data/chats';
-import ChatMessage from "../../components/ChatMessage";
-import InputTextBox from "../../components/InputTextBox";
-
-export default class ChatRoomScreen extends React.Component{
-  render() {
-    //const route = useRoute();
-    return(
-      <View style={styles.background}>
-        <FlatList
-          data={chats.messages}
-          renderItem={({ item }) => <ChatMessage message={item}/>}
-        />
-        <InputTextBox/>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create ({
-  background: {
-    backgroundColor: '#f3f3f3',
-    height: '100%'
-  }
-});
+// import React from 'react';
+// import { Text, FlatList, View, StyleSheet } from 'react-native';
+// import { useRoute } from '@react-navigation/native'
+// import chats from '../../../data/chats';
+// import ChatMessage from "../../../components/ChatMessage";
+// import InputTextBox from "../../../components/InputTextBox";
+//
+// export default class ChatBox extends React.Component{
+//   render() {
+//     //const route = useRoute();
+//     return(
+//       <View style={styles.background}>
+//         <FlatList
+//           data={chats.messages}
+//           renderItem={({ item }) => <ChatMessage message={item}/>}
+//         />
+//         <InputTextBox/>
+//       </View>
+//     );
+//   }
+// }
+//
+// const styles = StyleSheet.create ({
+//   background: {
+//     backgroundColor: '#f3f3f3',
+//     height: '100%'
+//   }
+// });
