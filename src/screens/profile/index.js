210e62c5405263b28d1141b28f223637ac73256b
Muhammad Wafa <iamprogrammercoder@gmail.com>
2021-06-22 13:39:16 +0430

    add place holder for image profile

--- src/screens/profile/index.js
+++ src/screens/profile/index.js
@@ -1,15 +1,27 @@
 import React, { useEffect } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 
-import { styles } from "../styles";
 
-export default ({navigation})=> {
+import { Ionicons, Entypo } from '@expo/vector-icons';
+
+import { styles } from "../styles";
+import { localStyle } from "./style";
+
+export default ({ navigation }) => {
 
 
     return (
         <View style={styles.container}>
-            <Text>Welcome to Profile</Text>
-            <TouchableOpacity onPress={()=> navigation.navigate("Settings")}>
+
+            <>
+                <TouchableOpacity style={localStyle.profileImageView}>
+                    <Entypo name="camera" size={45} color="grey" />
+                </TouchableOpacity>
+                <Text>Edit Profile Picture</Text>
+            </>
+
+
+            <TouchableOpacity onPress={() => navigation.navigate("Settings")}>
                 <Text>Go to Settings</Text>
             </TouchableOpacity>
         </View>
--- src/screens/profile/style.js
+++ src/screens/profile/style.js
@@ -1,9 +1,18 @@
 import { StyleSheet } from 'react-native';
 
 
-const styles = StyleSheet.create({
-
+const localStyle = StyleSheet.create({
+    profileImageView: {
+        width: 180,
+        height: 180,
+        borderRadius: 90,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth: 5,
+        borderColor: 'lightblue',
+        marginBottom: 10
+    }
 });
 
 
-module.exports = { styles };
+module.exports = { localStyle };
