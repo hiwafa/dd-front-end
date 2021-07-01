788fba81fb0084ea6586cd369a8ba52471da0fb2
Muhammad Wafa <iamprogrammercoder@gmail.com>
2021-07-01 10:35:08 +0430

    redux implmented and store configured for user

--- src/store/actions/UserSlice.js
+++ src/store/actions/UserSlice.js
@@ -1,2 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+
+const userSlice = createSlice({
+
+    name: "user",
+    initialState: {
+        firstname: null,
+        lastname: null,
+        email: null,
+        username: null,
+        password: null
+    },
+    reducers: {
+        login: (state, {payload}) => {
+
+        },
+        signup: (state, {payload}) => {
+
+        }
+    },
+    extraReducers: {
+
+    }
+});
+
+export const {} = userSlice.actions;
+
+export default userSlice.reducer;
