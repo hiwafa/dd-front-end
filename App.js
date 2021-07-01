0d67c425f8c04de170fcc7d17255eccad1337b9b
Muhammad Wafa <iamprogrammercoder@gmail.com>
2021-07-01 10:39:31 +0430

    store and provider figured out to root component

--- App.js
+++ App.js
@@ -1,22 +1,16 @@
 import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
 import Navigator from './src/Navigator';
+import store from "./src/store";
+import { Provider } from "react-redux";
 
 export default class App extends React.Component {
   render(){
       return (
-        <Navigator/>
+        <Provider store={store}>
+          <Navigator/>
+        </Provider>
       );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
