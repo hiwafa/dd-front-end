3244f869d6acc02e02ec5bd63a57655cf752282b
Muhammad Wafa <iamprogrammercoder@gmail.com>
2021-07-03 17:56:25 +0430

    added username field

--- src/screens/register/index.js
+++ src/screens/register/index.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import { ImageBackground, StyleSheet, Text, View, Image, TextInput } from 'react-native';
 
 export default ({ navigation: { navigate } }) => {
+
+  const [state, setState] = useReducer((_, v)=> v, {
+    username: null, password: null, email: null
+  });
 
   return (
     <View style={{ backgroundColor: 'black', height: '100%' }}>
@@ -19,6 +23,11 @@ export default ({ navigation: { navigate } }) => {
           <Text style={styles.fieldText}>Email address</Text>
           <View style={styles.field}>
             <TextInput placeholder='Enter your email' style={{ paddingHorizontal: 10, width: '100%' }} />
+          </View>
+
+          <Text style={styles.fieldText}>Username</Text>
+          <View style={styles.field}>
+            <TextInput placeholder='Enter your username' style={{ paddingHorizontal: 10, width: '100%' }} />
           </View>
 
           <Text style={styles.fieldText}>Password</Text>
@@ -50,7 +59,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     marginTop: 4,
-    marginHorizontal: 25,
+    marginHorizontal: 20,
     paddingHorizontal: 10,
     paddingVertical: 20,
     borderRadius: 16,
@@ -69,7 +78,7 @@ const styles = StyleSheet.create({
   fieldText: {
     color: 'black',
     marginHorizontal: 25,
-    marginTop: 50
+    marginTop: 20
   },
   header: {
     textAlign: 'center',
