725f5c1cafe50b74eec09cd5794d29080c94e55c
Muhammad Wafa <iamprogrammercoder@gmail.com>
2021-07-04 10:22:00 +0430

    login replaced with functional component

--- src/screens/login/index.js
+++ src/screens/login/index.js
@@ -1,108 +1,106 @@
 import React from 'react';
-import {ImageBackground, StyleSheet,Text,View,Image,TextInput} from 'react-native';
+import { ImageBackground, StyleSheet, Text, View, Image, TextInput } from 'react-native';
 
-export default class Login extends React.Component{
-    render(){
-        const {navigate} = this.props.navigation
-        return(
-            <View style={{backgroundColor:'black', height:'100%'}}>
-                <ImageBackground source={require('../../res/login_background_image.png')} style={{height:'100%'}}>
-                <Image source={require('../../res/dd_logo_whiteText_horizontal.svg')} style={styles.logo}/>
-                {/*BACKGROUND VIEW*/}
+export default ({ navigation: { navigate } }) => {
 
-                <View style={styles.loginContainer}>
-                    {/*LOGIN VIEW*/}
+  return (
+    <View style={{ backgroundColor: 'black', height: '100%' }}>
+      <ImageBackground source={require('../../res/login_background_image.png')} style={{ height: '100%' }}>
+        <Image source={require('../../res/dd_logo_whiteText_horizontal.svg')} style={styles.logo} />
+        {/*BACKGROUND VIEW*/}
 
-                    <Text style={styles.header}>Welcome,</Text>
-                    <Text style={styles.header}>Log in here</Text>
+        <View style={styles.loginContainer}>
+          {/*LOGIN VIEW*/}
 
-                    {/*Input fields*/}
-                    <Text style={styles.fieldText}>Email address</Text>
-                    <View style={styles.field}>
-                        <TextInput placeholder='Enter your email' style={{paddingHorizontal:10, width:'100%'}}/>
-                    </View>
+          <Text style={styles.header}>Welcome,</Text>
+          <Text style={styles.header}>Log in here</Text>
 
-                    <Text style={styles.fieldText}>Password</Text>
-                    <View style={styles.field}>
-                        <TextInput secureTextEntry placeholder='Enter your password' style={{paddingHorizontal:10, width:'100%'}}/>
-                    </View>
+          {/*Input fields*/}
+          <Text style={styles.fieldText}>Email address</Text>
+          <View style={styles.field}>
+            <TextInput placeholder='Enter your email' style={{ paddingHorizontal: 10, width: '100%' }} />
+          </View>
 
-                    {/*Buttons*/}
-                    <View style={styles.button}>
-                        <Text style={{color:'white'}}>
-                            Login
-                        </Text>
-                    </View>
+          <Text style={styles.fieldText}>Password</Text>
+          <View style={styles.field}>
+            <TextInput secureTextEntry placeholder='Enter your password' style={{ paddingHorizontal: 10, width: '100%' }} />
+          </View>
 
-                    <View style={{flexDirection: 'row', paddingVertical:'10%', marginTop: 20, marginHorizontal: 10}}>
-                        <Text onPress={() => navigate('Register')} style={styles.linkLeft}>
-                            Forgot password?
-                        </Text>
-                        <Text onPress={() => navigate('Register')} style={styles.linkRight}>
-                            Create account
-                        </Text>
-                    </View>
-                </View>
+          {/*Buttons*/}
+          <View style={styles.button}>
+            <Text style={{ color: 'white' }}>
+              Login
+            </Text>
+          </View>
 
-                </ImageBackground>
-            </View>
-        )
-    }
+          <View style={{ flexDirection: 'row', paddingVertical: '10%', marginTop: 20, marginHorizontal: 10 }}>
+            <Text onPress={() => navigate('Register')} style={styles.linkLeft}>
+              Forgot password?
+            </Text>
+            <Text onPress={() => navigate('Register')} style={styles.linkRight}>
+              Create account
+            </Text>
+          </View>
+        </View>
+
+      </ImageBackground>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
   field: {
-    flexDirection:'row',
-    alignItems:'center',
-    marginTop:4,
-    marginHorizontal:25,
-    paddingHorizontal:10,
-    paddingVertical:20,
-    borderRadius:16,
-    backgroundColor:'#F1F3F9'
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 4,
+    marginHorizontal: 25,
+    paddingHorizontal: 10,
+    paddingVertical: 20,
+    borderRadius: 16,
+    backgroundColor: '#F1F3F9'
   },
   button: {
-    marginHorizontal:25,
-    alignItems:'center',
-    justifyContent:'center',
-    marginTop:50,
-    backgroundColor:'#FF4773',
-    paddingVertical:20,
-    borderRadius:32
+    marginHorizontal: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 50,
+    backgroundColor: '#FF4773',
+    paddingVertical: 20,
+    borderRadius: 32
   },
   linkRight: {
-    color:'#929292',
-    textDecorationLine:'underline',
+    color: '#929292',
+    textDecorationLine: 'underline',
     position: 'absolute',
     right: 25
   },
   linkLeft: {
-    color:'#929292',
-    textDecorationLine:'underline',
+    color: '#929292',
+    textDecorationLine: 'underline',
     position: 'absolute',
     left: 25
   },
   fieldText: {
-    color:'black',
-    marginHorizontal:25,
-    marginTop:50
+    color: 'black',
+    marginHorizontal: 25,
+    marginTop: 50
   },
   header: {
-    textAlign:'center',
-    color:'black',
+    textAlign: 'center',
+    color: 'black',
     fontSize: 36,
     fontWeight: "bold"
   },
   logo: {
-    marginVertical:50,
-    marginHorizontal:75,
-    height:'4%',
+    marginVertical: 50,
+    marginHorizontal: 75,
+    height: '4%',
     resizeMode: 'contain'
   },
   loginContainer: {
-    backgroundColor:'#fff',
-    borderRadius:16,
-    marginHorizontal:55,
-    paddingTop:40
+    backgroundColor: '#fff',
+    borderRadius: 16,
+    marginHorizontal: 55,
+    paddingTop: 40
   },
 });
