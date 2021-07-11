1ab7cfa2318ed1f537c0d6f298ad913e7bb871ca
Muhammad Wafa <iamprogrammercoder@gmail.com>
2021-07-11 20:03:10 +0430

    messages component updated

--- src/screens/chatbox/index.js
+++ src/screens/chatbox/index.js
@@ -19,32 +19,47 @@ export default function (props) {
 
 
 
-export class ChatBox extends React.Component {
-  render() {
-    const headerHeight = this.props.headerHeight;
-    const viewHeight = windowHeight - headerHeight;
-    const route = this.props.route;
-    return (
-      <View style={[styles.background, { height: viewHeight }]}>
-        
-        <FlatList
-          ref={(ref) => this.scrollView = ref}
-          onContentSizeChange={() => { this.scrollView.scrollToEnd({ animated: true }) }}
-          style={{ height: viewHeight - headerHeight }}
-          data={chats.messages}
-          renderItem={({ item }) => <ChatMessage message={item} />}
-        />
+export const ChatBox = ({headerHeight, route}) => {
 
-        <KeyboardAvoidingView
+  
+  const viewHeight = windowHeight - headerHeight;
+  const route = route;
+
+
+  useEffect(()=> {
+
+    const interval = setInterval(async ()=> {
+
+      dispatch(fetchMessages({token, chatId: chatRoom.id}));
+
+    }, 1000);
+
+    return ()=> {
+      clearInterval(interval);
+    }
+
+  }, []);
+
+  return (
+    <View style={[styles.background, { height: viewHeight }]}>
+
+      <FlatList
+        ref={(ref) => this.scrollView = ref}
+        onContentSizeChange={() => { this.scrollView.scrollToEnd({ animated: true }) }}
+        style={{ height: viewHeight - headerHeight }}
+        data={chats.messages}
+        renderItem={({ item }) => <ChatMessage message={item} />}
+      />
+
+      <KeyboardAvoidingView
+        style={{ position: 'absolute', left: 0, right: 0, bottom: 0 }}
+        behavior="position">
+        <InputTextBox
           style={{ position: 'absolute', left: 0, right: 0, bottom: 0 }}
-          behavior="position">
-          <InputTextBox
-            style={{ position: 'absolute', left: 0, right: 0, bottom: 0 }}
-            behavior="position" />
-        </KeyboardAvoidingView>
-      </View>
-    );
-  }
+          behavior="position" />
+      </KeyboardAvoidingView>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
