21f02d1ca88e550ed2d79f2d2670da9395a7b13b
Muhammad Wafa <iamprogrammercoder@gmail.com>
2021-07-17 11:49:09 +0430

    navigation go back added to header

--- src/components/UserImage.js
+++ src/components/UserImage.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
-import {View, Text, Image, TouchableOpacity} from 'react-native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 
-export default ({userImage})=> {
+export default ({ userImage }) => {
 
+  const { navigate, goBack } = useNavigation();
 
+  return (
+    <View style={{ flexDirection: 'row' }}>
+      <TouchableOpacity style={{ marginLeft: 10, marginTop: 10 }}
+        onPress={() => {
+          goBack();
+        }}>
+        <AntDesign name="arrowleft" size={24} color="black" />
+      </TouchableOpacity>
 
-    return (
-      <View style={{flexDirection: 'row'}}>
-        <TouchableOpacity style={{marginLeft: 10, marginTop: 10}}>
-          <AntDesign name="arrowleft" size={24} color="black" />
-        </TouchableOpacity>
-        
-        <Image
-          source={{uri: userImage}}
-          style={{width: 40, height: 40, borderRadius: 40/2, marginLeft: 15}}
-        />
-      </View>
-    );
+      <Image
+        source={{ uri: userImage }}
+        style={{
+          width: 40, height: 40,
+          borderRadius: 40 / 2, marginLeft: 15
+        }}
+      />
+    </View>
+  );
 
 }
