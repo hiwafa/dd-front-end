24870ba124483cb4e23421a3c5b059b8231d1b8f
Muhammad Wafa <iamprogrammercoder@gmail.com>
2021-07-30 17:58:11 +0430

    update main app 2

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 
+
 import Navigator from './src/Navigator';
-import store from "./src/store";
 import { Provider } from "react-redux";
+import store from "./src/store";
 
 export default () => (
   <Provider store={store}>
