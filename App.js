14f9037f3f949518f7c23741c57b39fef0c059aa
Muhammad Wafa <iamprogrammercoder@gmail.com>
2021-07-30 17:57:27 +0430

    update main app

--- App.js
+++ App.js
@@ -5,12 +5,8 @@ import Navigator from './src/Navigator';
 import store from "./src/store";
 import { Provider } from "react-redux";
 
-export default class App extends React.Component {
-  render(){
-      return (
-        <Provider store={store}>
-          <Navigator/>
-        </Provider>
-      );
-  }
-}
+export default () => (
+  <Provider store={store}>
+    <Navigator />
+  </Provider>
+);
